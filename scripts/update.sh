7727f794f0b58c9abcc3e945a6d902239c2f5fbb
Weston Steimel <weston.steimel@gmail.com>
2021-08-12 11:56:16 +0100

    update to github actions to perform all updates before building images
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- scripts/update.sh
+++ scripts/update.sh
@@ -12,3 +12,15 @@ sed -ri \
 git add stable/Dockerfile
 git diff-index --quiet HEAD || git commit --message "updated stable to version ${version}"
 
+
+version=`curl -L -s "https://www.torproject.org/download/alpha/" | pcregrep -o1 -e "\/ <a href=\"\/dist\/torbrowser\/(.*)\/tor-browser-linux64-.*_en-US.tar.xz\">64-bit<\/a>"`
+
+echo "latest alpha version: ${version}"
+
+sed -ri \
+    -e 's/^(ARG VERSION=).*/\1'"\"${version}\""'/' \
+    "alpha/Dockerfile"
+
+git add alpha/Dockerfile
+git diff-index --quiet HEAD || git commit --message "updated alpha to version ${version}"
+

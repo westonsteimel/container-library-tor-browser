53e4b2719a066734565ca2cfeda260ef0a11478b
Weston Steimel <weston.steimel@gmail.com>
2022-12-21 15:44:25 +0000

    fix: update links in update script
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-version=`curl -L -s "https://www.torproject.org/download/" | pcregrep -o1 -e "href=\"\/dist\/torbrowser\/(.*)/tor-browser-linux64-.*_en-US.tar.xz\">Download for Linux<\/a>"`
+version=`curl -L -s "https://www.torproject.org/download/" | pcregrep -o1 -e "href=\"\/dist\/torbrowser\/(.*)/tor-browser-linux64-.*_ALL.tar.xz\">Download for Linux<\/a>"`
 echo "latest stable version: ${version}"
 
 sed -ri \
@@ -13,7 +13,7 @@ git add stable/Dockerfile
 git diff-index --quiet HEAD || git commit --message "updated stable to version ${version}"
 
 
-version=`curl -L -s "https://www.torproject.org/download/alpha/" | pcregrep -o1 -e "\/ <a href=\"\/dist\/torbrowser\/(.*)\/tor-browser-linux64-.*_en-US.tar.xz\">64-bit<\/a>"`
+version=`curl -L -s "https://www.torproject.org/download/alpha/" | pcregrep -o1 -e "\/ <a href=\"\/dist\/torbrowser\/(.*)\/tor-browser-linux64-.*_ALL.tar.xz\">64-bit<\/a>"`
 
 echo "latest alpha version: ${version}"
 
